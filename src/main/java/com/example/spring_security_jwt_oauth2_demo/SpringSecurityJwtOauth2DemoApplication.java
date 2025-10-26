0b216c8adaa35b562f100e929e15684a53827880
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-26 08:03:48 -0300

    first commit

---
+++ src/main/java/com/example/spring_security_jwt_oauth2_demo/SpringSecurityJwtOauth2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_jwt_oauth2_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtOauth2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtOauth2DemoApplication.class, args);
+	}
+
+}
